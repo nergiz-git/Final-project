601dcf4cf867609ad510eedcaddfa5f4c741e3a2
Nərgiz <nergizmehdiyeva051@gmail.com>
2026-03-22 20:57:42 +0400

    commits

--- .history/ui/src/main_20260130234807.jsx
+++ .history/ui/src/main_20260130234807.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import {legacy_createStore} from 'redux'
 const store=legacy_createStore(Reducer)
 import { Provider } from 'react-redux'
-import Reducer from "./store/Reducer";
+import Reducer from "./store/reducer";
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
       <App />
